8114384c6a5e9ec4a41dfee66e5ccf3265294377
mini-chip <75009853+mini-chip@users.noreply.github.com>
2024-07-16 21:34:33 +0900

    장바구니 기능 구현  (#7)
    * feat: Cart파일 생성
    
    * feat: 장바구니 페이지 및 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProductList from './page/ProductList'
-import './App.css'
 import ProductDetail from './page/ProductDetail'
-
+import CartPage from './page/CartPage'
+import { CartProvider } from './context/CartContext.jsx'
+import './App.css'
 export default function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<ProductList />} />
-                <Route path="products">
-                    <Route path=":productId" element={<ProductDetail />} />
-                </Route>
-            </Routes>
-        </Router>
+        <CartProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<ProductList />} />
+                    <Route path="products">
+                        <Route path=":productId" element={<ProductDetail />} />
+                    </Route>
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </Router>
+        </CartProvider>
     )
 }
