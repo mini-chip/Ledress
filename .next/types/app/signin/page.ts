ab2a423be0a9ff56d49401bd56548a36ce220269
minhee <kimmin5209@gmail.com>
2025-02-05 01:11:29 +0900

    feat: signin css

--- .next/types/app/signin/page.ts
+++ .next/types/app/signin/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/kimminhee/Desktop/WED/src/app/signin/page.tsx
+// File: C:\Users\PC\Documents\GitHub\Ledress\src\app\signin\page.tsx
 import * as entry from '../../../../src/app/signin/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
